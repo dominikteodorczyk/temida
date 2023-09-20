3ca89e21b618d3f65c994b2b5e345e60dc3fd3c0
Dominik <dominik.teodorczyk1@gmail.com>
2023-09-20 20:08:53 +0200

    added docstrings

--- src/utils/events.py
+++ src/utils/events.py
@@ -1,12 +1,26 @@
+"""
+A module containing classes of objects representing events.
+"""
+
 from pandas import DataFrame
 
 
 class TwoWayBetEvent:
+    """
+    The object of a single event obtained by scraping data
+    at all bookmakers.
+    """
+
     def __init__(self) -> None:
         pass
 
 
 class TwoWayBetEventsTable:
+    """
+    Table of events obtained when scratching a given sport
+    at a single bookmaker.
+    """
+
     def __init__(self) -> None:
         self.events_list = DataFrame(
             columns=[
@@ -19,10 +33,18 @@ class TwoWayBetEventsTable:
         )
 
     def imput_event(self, event_data: dict):
+        """
+        Method of adding event objects when condensing all events
+        of a given sport at a given bookmaker into a table.
+        """
         self.events_list._append({})
 
 
 class Event:
+    """
+    Values of odds for a given event at all bookmakers.
+    """
+
     def __init__(self, home_player, away_player) -> None:
         self.home_player = home_player
         self.away_player = away_player
@@ -37,4 +59,8 @@ class Event:
         return f"{self.home_player}_&_{self.away_player}"
 
     def make_bets_table(self):
+        """
+        Method that retrieves data for an event from individual
+        bookmaker tables.
+        """
         pass
