e42d26d3e2ea24b0cae3260dac2810db7ad7f27a
Dominik <dominik.teodorczyk1@gmail.com>
2023-10-01 21:39:46 +0200

    Create two -way scraper class for Superbet [002_SUPBET]

--- src/scrapers/superbet.py
+++ src/scrapers/superbet.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class SuperbetScraper(Scraper):
     def __init__(self, site_path: str) -> None:
         super().__init__(site_path)
-        self.events_objects: dict = {}
+        self.events_objects: list = []
 
     def close_cookie_msg(self):
         try:
@@ -43,34 +43,54 @@ class SuperbetScraper(Scraper):
     def get_segments(self):
         pass
 
-    def get_all_events_objects(self):
-        height = self.driver.execute_script("return window.scrollY;")
-        self.driver.execute_script("window.scrollTo(0, 0);")
-        all_elements = []
-        while True:
-            elements = self.driver.find_elements(
-                By.XPATH, '//*[contains(@id, "event-")]/div/div[1]')
-            for element in elements:
-                home = element.find_element(
-                By.XPATH, './div[1]/div[2]/div[1]/span[1]').text
-                away = element.find_element(
-                By.XPATH, './div[1]/div[2]/div[1]/span[2]').text
-                self.events_objects[f'{home}{away}'] = element
-            self.driver.execute_script(f"window.scrollTo(0, window.scrollY + {4000});")
-            time.sleep(0.2)
-            new_height = self.driver.execute_script("return window.scrollY;")
-            if height == new_height:
-                break
-            height = new_height
-        print(len(self.events_objects))
-
     def get_events_from_site(self):
         self.close_cookie_msg()
         self.get_whole_site()
         self.get_whole_site()
         self.get_segments()
-        self.get_all_events_objects()
         # except Exception as e:
         #     print(e)
 
+class SuperbetTwoWayBets(SuperbetScraper):
+    def __init__(self, site_path: str) -> None:
+        super().__init__(site_path)
+        self.get_events_from_site()
+
+    def get_all_events_objects(self):
+        height = self.driver.execute_script("return window.scrollY;")
+        self.driver.execute_script("window.scrollTo(0, 0);")
+        while True:
+            elements = self.driver.find_elements(
+                By.XPATH, '//*[contains(@id, "event-")]/div/div[1]')
+            for element in elements:
+                try:
+                    home = element.find_element(
+                    By.XPATH, './div[1]/div[2]/div[1]/span[1]').text
+                    away = element.find_element(
+                    By.XPATH, './div[1]/div[2]/div[1]/span[2]').text
+                    if f'{home}{away}' in self.events_objects: pass
+                    else:
+                        home_team_win = element.find_element(
+                            By.XPATH,
+                            "./div[2]/div[2]/div/div[1]/button/span[4]/span[2]",
+                        ).text
+                        away_team_win = element.find_element(
+                            By.XPATH,
+                            "./div[2]/div[2]/div/div[2]/button/span[4]/span[2]",
+                        ).text
+                        event_date = element.find_element(
+                            By.XPATH,
+                            "./div[1]/div[1]/span[1]",
+                        ).text
+                        print(home, away, home_team_win, away_team_win, event_date)
+                        self.events_objects.append(f'{home}{away}')
+                except Exception as e:
+                    print(e)
+            self.driver.execute_script(f"window.scrollTo(0, window.scrollY + {4000});")
+            time.sleep(0.01)
+            new_height = self.driver.execute_script("return window.scrollY;")
+            if height == new_height:
+                break
+            height = new_height
+        print(len(self.events_objects))
 
