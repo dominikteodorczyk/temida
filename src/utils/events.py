34c0369a1e5cbb548656efbcd667638f83fce0cf
Dominik <dominik.teodorczyk1@gmail.com>
2023-10-02 16:49:04 +0200

    Create a class of event facilities and two- and three-way events [015_CORUTI]

--- src/utils/events.py
+++ src/utils/events.py
@@ -4,38 +4,126 @@ A module containing classes of objects representing events.
 
 from pandas import DataFrame
 
+
 class BetEvent:
-    def __init__(self, home_player, away_player,event_date) -> None:
+    def __init__(
+        self, event_name, home_player, away_player, event_date
+    ) -> None:
+        self.event_name = event_name
         self.home_player = home_player
         self.away_player = away_player
         self.event_date = event_date
 
-    @classmethod
-    def input(cls, data_list):
-        new_bet = cls(data_list)
-        return new_bet
 
 class TwoWayBetEvent(BetEvent):
     """
     The object of a single two-way event obtained by scraping data
     at all bookmakers.
     """
-    def __init__(self, home_player, away_player, event_date, home_team_win, away_team_win) -> None:
-        super().__init__(home_player, away_player, event_date)
+
+    def __init__(
+        self,
+        event_name,
+        home_player,
+        away_player,
+        event_date,
+        home_team_win,
+        away_team_win,
+    ) -> None:
+        super().__init__(
+            event_name,
+            home_player,
+            away_player,
+            event_date,
+        )
         self.home_team_win = home_team_win
         self.away_team_win = away_team_win
 
+    def to_dataframe(self):
+        data = {
+            "Event Name": [self.event_name],
+            "Home Team": [self.home_player],
+            "Away Team": [self.away_player],
+            "Date": [self.event_date],
+            "Home Win": [self.home_team_win],
+            "Away Win": [self.away_team_win],
+        }
+        return DataFrame(data)
 
+    @classmethod
+    def create_from_data(cls, bukmacher_data, parser):
+        home_player = parser.parse_home_name(bukmacher_data.get["home_team"])
+        away_player = parser.parse_away_name(bukmacher_data.get["away_team"])
+        event_name = parser.parse_event_name(
+            bukmacher_data.get("home_team", "away_team")
+        )
+        event_date = parser.parse_date(bukmacher_data["date"])
+        home_team_win = parser.parse_home_win(bukmacher_data["home_win"])
+        away_team_win = parser.parse_away_win(bukmacher_data["away_win"])
+        return cls(
+            event_name,
+            home_player,
+            away_player,
+            event_date,
+            home_team_win,
+            away_team_win,
+        )
+
+
+# Przykład użycia
 class ThreeWayBetEvent(BetEvent):
     """
     The object of a single three-way event obtained by scraping data
     at all bookmakers.
     """
-    def __init__(self, home_player, away_player, event_date, home_team_win, away_team_win, draw) -> None:
-        super().__init__(home_player, away_player, event_date)
+
+    def __init__(
+        self,
+        event_name,
+        home_player,
+        away_player,
+        event_date,
+        home_team_win,
+        draw,
+        away_team_win,
+    ) -> None:
+        super().__init__(event_name, home_player, away_player, event_date)
         self.home_team_win = home_team_win
         self.away_team_win = away_team_win
         self.draw = draw
+
+    def to_dataframe(self):
+        data = {
+            "Event Name": [self.event_name],
+            "Home Team": [self.home_player],
+            "Away Team": [self.away_player],
+            "Date": [self.event_date],
+            "Home Win": [self.home_team_win],
+            "Draw": [self.draw],
+            "Away Win": [self.away_team_win],
+        }
+        return DataFrame(data)
+
+    @classmethod
+    def create_from_data(cls, bukmacher_data, parser):
+        home_player = parser.parse_home_name(bukmacher_data.get["home_team"])
+        away_player = parser.parse_away_name(bukmacher_data.get["away_team"])
+        event_name = parser.parse_event_name(
+            bukmacher_data.get("home_team", "away_team")
+        )
+        event_date = parser.parse_date(bukmacher_data["date"])
+        home_team_win = parser.parse_home_win(bukmacher_data["home_win"])
+        draw = parser.parse_draw(bukmacher_data["draw"])
+        away_team_win = parser.parse_away_win(bukmacher_data["away_win"])
+        return cls(
+            event_name,
+            home_player,
+            away_player,
+            event_date,
+            home_team_win,
+            draw,
+            away_team_win,
+        )
 
 
 class TwoWayBetEventsTable:
@@ -71,11 +159,8 @@ class Event:
     def __init__(self, home_player, away_player) -> None:
         self.home_player = home_player
         self.away_player = away_player
-        self.event_bets_values = DataFrame(columns=[
-            "bookmaker",
-            "home",
-            "away"
-            ]
+        self.event_bets_values = DataFrame(
+            columns=["bookmaker", "home", "away"]
         )
 
     def __repr__(self) -> str:
