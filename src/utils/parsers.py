d117245d13967b22deb2dd292c843fdb5368cb04
Dominik <dominik.teodorczyk1@gmail.com>
2023-10-02 16:07:48 +0200

    Create a STS parsers class [012_CORUTI]

--- src/utils/parsers.py
+++ src/utils/parsers.py
@@ -37,11 +37,15 @@ class FortunaParser(Parser):
 
     @staticmethod
     def parse_date(date_str):
-        return datetime.strptime(f"{date_str}.{datetime.now().year}", "%d.%m.%Y")
+        today = datetime.now().date()
+        if today.month >= datetime.strptime(date_str, "%d.%m.%Y").month:
+            return datetime.strptime(f"{date_str}.{datetime.now().year}", "%d.%m.%Y")
+        else:
+            return datetime.strptime(f"{date_str}.{datetime.now().year + 1}", "%d.%m.%Y")
 
     @staticmethod
-    def parse_event_name(event_name):
-        return event_name.strip()
+    def parse_event_n_names(*args):
+        return args[0].strip()
 
     @staticmethod
     def parse_home_name(event_name):
@@ -50,6 +54,27 @@ class FortunaParser(Parser):
     @staticmethod
     def parse_away_name(event_name):
         return event_name.split(" - ")[1].strip().upper()
+
+
+class STSParser(Parser):
+
+    @staticmethod
+    def parse_date(date_str):
+        return datetime.strptime(f"{date_str}", "%d.%m.%Y")
+
+    @staticmethod
+    def parse_event_n_names(*args):
+        return f'{args[0].strip()} - {args[1].strip()}'
+
+    @staticmethod
+    def parse_home_name(home_name):
+        return home_name.strip().upper()
+
+    @staticmethod
+    def parse_away_name(away_name):
+        return away_name.strip().upper()
+
+
 
 # :TODO: propozycja algorytmu najdującego podobieństwo
 # def jaccard_similarity(s1, s2):
@@ -107,78 +132,3 @@ class FortunaParser(Parser):
 # print best
 
 # potrafi połączyć 'Crvena Zvezda - Young Boys' z 'CZ Belgrad - YB Bern'
-
-import pandas as pd
-from datetime import datetime
-
-class Parser:
-    @staticmethod
-    def parse_date(date_str):
-        return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
-
-    @staticmethod
-    def parse_event_name(home_team, away_team):
-        if home_team and away_team:
-            return f"{home_team} vs {away_team}"
-        elif home_team:
-            return f"{home_team} Match"
-        elif away_team:
-            return f"{away_team} Match"
-        else:
-            return "Unknown Event"
-
-    @staticmethod
-    def parse_odds(odds_str):
-        return float(odds_str)
-
-class Event:
-    def __init__(self, event_name, home_team, away_team, date, odds_win, odds_loss):
-        self.event_name = event_name
-        self.home_team = home_team
-        self.away_team = away_team
-        self.date = date
-        self.odds_win = odds_win
-        self.odds_loss = odds_loss
-
-    def __str__(self):
-        return f"{self.event_name}: {self.home_team} vs {self.away_team}, Date: {self.date}, Win Odds: {self.odds_win}, Loss Odds: {self.odds_loss}"
-
-    def to_dataframe(self):
-        data = {
-            'Event Name': [self.event_name],
-            'Home Team': [self.home_team],
-            'Away Team': [self.away_team],
-            'Date': [self.date],
-            'Win Odds': [self.odds_win],
-            'Loss Odds': [self.odds_loss]
-        }
-        return pd.DataFrame(data)
-
-    @classmethod
-    def create_from_bukmacher_data(cls, bukmacher_data, parser):
-        home_team = bukmacher_data.get('home_team', None)
-        away_team = bukmacher_data.get('away_team', None)
-        event_name = bukmacher_data.get('event_name', None) or parser.parse_event_name(home_team, away_team)
-
-        date = parser.parse_date(bukmacher_data['date'])
-        odds_win = parser.parse_odds(bukmacher_data['odds_win'])
-        odds_loss = parser.parse_odds(bukmacher_data['odds_loss'])
-
-        return cls(event_name, home_team, away_team, date, odds_win, odds_loss)
-
-# Przykład użycia
-bukmacher_data = {
-    'event_name': 'Special Event',
-    'home_team': 'PlayerX',
-    'away_team': 'PlayerY',
-    'date': '2023-09-28 18:30:00',
-    'odds_win': '1.8',
-    'odds_loss': '2.2'
-}
-
-parser = Parser()
-event = Event.create_from_bukmacher_data(bukmacher_data, parser)
-
-# Przekształcenie obiektu do DataFrame
-df = event.to_dataframe()
-print(df)
