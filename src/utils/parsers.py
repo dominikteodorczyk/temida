fd6b96c9fc93f277c0a12f9c637b7a0fc7b143b8
Dominik <dominik.teodorczyk1@gmail.com>
2023-10-02 19:37:00 +0200

    date format fixing

--- src/utils/parsers.py
+++ src/utils/parsers.py
@@ -1,50 +1,35 @@
 from datetime import datetime, timedelta
 
-class Parser:
 
+class Parser:
     @staticmethod
     def parse_home_win(odds_str):
-        return float(odds_str)
+        return float(odds_str.replace(",", "."))
 
     @staticmethod
     def parse_draw(odds_str):
-        return float(odds_str)
+        return float(odds_str.replace(",", "."))
 
     @staticmethod
     def parse_away_win(odds_str):
-        return float(odds_str)
+        return float(odds_str.replace(",", "."))
 
-class STSParsers:
-
-    def __init__(self) -> None:
-        pass
-
-    def parse_home(self,home_name:str) -> str:
-        components = home_name.split()
-        return max(components, key=len).strip().upper()
-
-    def parse_away(self,away_name:str) -> str:
-        components = away_name.split()
-        return max(components, key=len).strip().upper()
-
-    def parse_date(self,event_date:str) -> str:
-        pass
-
-    def parse_event_code(self,even_name:str,event_date:str) -> str:
-        pass
 
 class FortunaParser(Parser):
-
     @staticmethod
     def parse_date(date_str):
         today = datetime.now().date()
-        if today.month <= datetime.strptime(date_str, "%d.%m.%Y").month:
-            return datetime.strptime(f"{date_str}.{datetime.now().year}", "%d.%m.%Y")
+        if today.month <= datetime.strptime(date_str[:5], "%d.%m").month:
+            return datetime.strptime(
+                f"{date_str[:5]}.{datetime.now().year}", "%d.%m.%Y"
+            )
         else:
-            return datetime.strptime(f"{date_str}.{datetime.now().year + 1}", "%d.%m.%Y")
+            return datetime.strptime(
+                f"{date_str[:5]}.{datetime.now().year + 1}", "%d.%m.%Y"
+            )
 
     @staticmethod
-    def parse_event_n_names(*args):
+    def parse_event_name(*args):
         return args[0].strip()
 
     @staticmethod
@@ -57,14 +42,16 @@ class FortunaParser(Parser):
 
 
 class STSParser(Parser):
-
     @staticmethod
     def parse_date(date_str):
-        return datetime.strptime(f"{date_str}", "%d.%m.%Y")
+        if date_str == "Dzisiaj":
+            return datetime.now().strftime("%Y-%m-%d")
+        else:
+            return datetime.strptime(f"{date_str}", "%d.%m.%Y")
 
     @staticmethod
-    def parse_event_n_names(*args):
-        return f'{args[0].strip()} - {args[1].strip()}'
+    def parse_event_name(*args):
+        return f"{args[0].strip()} - {args[1].strip()}"
 
     @staticmethod
     def parse_home_name(home_name):
@@ -76,21 +63,20 @@ class STSParser(Parser):
 
 
 class BetclicParser(Parser):
-
     @staticmethod
     def parse_date(date_str):
-        if date_str == 'Dzisiaj':
-            return datetime.now().strftime("%d.%m.%Y")
-        elif date_str == 'Jutro':
-            return (datetime.now() + timedelta(days=1)).strftime("%d.%m.%Y")
-        elif date_str == 'Pojutrze':
-            return (datetime.now() + timedelta(days=2)).strftime("%d.%m.%Y")
+        if date_str == "Dzisiaj":
+            return datetime.now().strftime("%Y-%m-%d")
+        elif date_str == "Jutro":
+            return (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+        elif date_str == "Pojutrze":
+            return (datetime.now() + timedelta(days=2)).strftime("%Y-%m-%d")
         else:
             return datetime.strptime(f"{date_str}", "%d.%m.%Y")
 
     @staticmethod
-    def parse_event_n_names(*args):
-        return f'{args[0].strip()} - {args[1].strip()}'
+    def parse_event_name(*args):
+        return f"{args[0].strip()} - {args[1].strip()}"
 
     @staticmethod
     def parse_home_name(home_name):
@@ -100,26 +86,42 @@ class BetclicParser(Parser):
     def parse_away_name(away_name):
         return away_name.strip().upper()
 
-class SuperbetParser(Parser):
 
+class SuperbetParser(Parser):
     @staticmethod
     def parse_date(date_str):
         current_date = datetime.now().date()
-        if date_str in ['PON.', 'WT.', 'ŚR.', 'CZW.', 'PT.', 'SOB.', 'NIEDZ.']:
-            weekday_mapping = {'PON.': 0, 'WT.': 1, 'ŚR.': 2, 'CZW.': 3, 'PT.': 4, 'SOB.': 5, 'NIEDZ.': 6}
-            days_until_next_weekday = (weekday_mapping[date_str] - current_date.weekday() + 7) % 7
-            next_weekday = current_date + timedelta(days=days_until_next_weekday)
-            return next_weekday.strftime("%d.%m.%Y")
+        if date_str in ["PON.", "WT.", "ŚR.", "CZW.", "PT.", "SOB.", "NIEDZ."]:
+            weekday_mapping = {
+                "PON.": 0,
+                "WT.": 1,
+                "ŚR.": 2,
+                "CZW.": 3,
+                "PT.": 4,
+                "SOB.": 5,
+                "NIEDZ.": 6,
+            }
+            days_until_next_weekday = (
+                weekday_mapping[date_str] - current_date.weekday() + 7
+            ) % 7
+            next_weekday = current_date + timedelta(
+                days=days_until_next_weekday
+            )
+            return next_weekday.strftime("%Y-%m-%d")
         else:
             today = datetime.now().date()
-            if today.month <= datetime.strptime(date_str, "%d.%m.%Y").month:
-                return datetime.strptime(f"{date_str}.{datetime.now().year}", "%d.%m.%Y")
+            if today.month <= datetime.strptime(date_str, "%d.%m").month:
+                return datetime.strptime(
+                    f"{date_str}.{datetime.now().year}", "%d.%m.%Y"
+                )
             else:
-                return datetime.strptime(f"{date_str}.{datetime.now().year + 1}", "%d.%m.%Y")
+                return datetime.strptime(
+                    f"{date_str}.{datetime.now().year + 1}", "%d.%m.%Y"
+                )
 
     @staticmethod
-    def parse_event_n_names(*args):
-        return f'{args[0].strip()} - {args[1].strip()}'
+    def parse_event_name(*args):
+        return f"{args[0].strip()} - {args[1].strip()}"
 
     @staticmethod
     def parse_home_name(home_name):
@@ -128,7 +130,6 @@ class SuperbetParser(Parser):
     @staticmethod
     def parse_away_name(away_name):
         return away_name.strip().upper()
-
 
 
 # :TODO: propozycja algorytmu najdującego podobieństwo
