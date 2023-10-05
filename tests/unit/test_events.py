0bcc82479b405ed329cfb9456fc27cdf46d5204a
Dominik <dominik.teodorczyk1@gmail.com>
2023-10-05 20:37:55 +0200

    Add test data for arbitrage and data prep. [003_UNITES]

---
+++ tests/unit/test_events.py
@@ -0,0 +1,41 @@
+import os
+import pytest
+import pandas as pd
+from utils.events import MainEventsBoard
+
+
+class Test_MainEventsBoard:
+    @pytest.fixture
+    def data_dir(self):
+        return os.path.join(os.getcwd(), "test", "data")
+
+    @pytest.fixture
+    def fortuna_board(self, data_dir):
+        return pd.read_csv(os.path.join(data_dir, "fortuna_data.csv"))
+
+    @pytest.fixture
+    def sts_board(self, data_dir):
+        return pd.read_csv(os.path.join(data_dir, "sts_data.csv"))
+
+    @pytest.fixture
+    def betclic_board(self, data_dir):
+        return pd.read_csv(os.path.join(data_dir, "betclic_data.csv"))
+
+    @pytest.fixture
+    def superbet_board(self, data_dir):
+        return pd.read_csv(os.path.join(data_dir, "superbet_data.csv"))
+
+    def test_MainEventsBoard_have_events_list_atribut_as_list(self):
+        atributes = vars(MainEventsBoard())
+        assert type(atributes["events_list"]) == list
+
+    def test_MainEventsBoard_have_events_table_atribut_as_DataFrame(self):
+        atributes = vars(MainEventsBoard())
+        assert type(atributes["events_table"]) == pd.DataFrame
+
+    # def test_MainEventsBoard_collect_data_in_list(
+    #         self,
+    #         fortuna_board,
+    #         sts_board,
+    #         betclic_board,
+    #         superbet_board):
