6d4aaf64742b6602bd9d998438da6aef58e305f4
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-10-07 18:20:33 +0200

    Create a events main board [023_CORUTI]

--- src/utils/events.py
+++ src/utils/events.py
@@ -3,6 +3,8 @@ A module containing classes of objects representing events.
 """
 
 from pandas import DataFrame
+from numpy import nan, mean
+from copy import deepcopy
 
 
 class BetEvent:
@@ -180,9 +182,78 @@ class MainEventsBoard:
     """
 
     def __init__(self) -> None:
-        self.events_list:list = []
-        self.events_table =  DataFrame()
+        self.events_dict: dict = {}
+        self.events_table = DataFrame()
 
     def put_data(self, data):
-        self.events_list.append(data)
+        self.events_dict[data.__str__()] = data
 
+    def get_cols_names(self):
+        cols_list = []
+        for key,value in self.events_dict.items():
+            cols_list.append(key)
+        return cols_list
+
+    def get_unique_dates(self):
+        date_list = []
+        for key, events_object in self.events_dict.items():
+            events_dates = events_object.data["event_date"].to_list()
+            for date in events_dates:
+                date_list.append(date)
+        return list(set(date_list))
+
+    def create_provisor_dict(self, date):
+        provisor_events_list = {}
+        for key, events_object in self.events_dict.items():
+            events_names = events_object.data.loc[
+                events_object.data["event_date"] == date, "event_name"
+            ]
+            provisor_events_list[key] = events_names.to_list()
+        return provisor_events_list
+
+    def highest_number_of_records(self, data):
+        return max(data, key=lambda k: len(data[k]))
+
+    def jaccard_similarity(self, s1, s2):
+        set1 = set(s1)
+        set2 = set(s2)
+        intersection = len(set1.intersection(set2))
+        union = len(set1) + len(set2) - intersection
+        similarity = intersection / union
+        return similarity
+
+    def create_events_table(self):
+        self.events_table = DataFrame(columns=self.get_cols_names())
+        dates_list = self.get_unique_dates()
+        for date in dates_list:
+            work_dict = self.create_provisor_dict(date)
+            main_list = work_dict[self.highest_number_of_records(work_dict)]
+            rest_dict = deepcopy(work_dict)
+            del rest_dict[self.highest_number_of_records(work_dict)]
+            for main_event in main_list:
+                temporal_dict = {}
+                for key, value in rest_dict.items():
+                    ratio = 0
+                    best = None
+                    for secound_event in value:
+                        try:
+                            similarity = self.jaccard_similarity(
+                                main_event, secound_event
+                            )
+                            if similarity > 0.75:
+                                if similarity > ratio:
+                                    best = secound_event
+                                    ratio = similarity
+                        except:
+                            best = None
+                            pass
+                    temporal_dict[key] = best
+                temporal_dict[
+                    self.highest_number_of_records(work_dict)
+                ] = main_event
+                self.events_table = self.events_table._append(
+                    temporal_dict, ignore_index=True
+                )
+        mask = self.events_table.count(axis=1) == 1
+        self.events_table = self.events_table[~mask]
+        return self.events_table
