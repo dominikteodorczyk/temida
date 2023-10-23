08be46a5cd11508402ec52043057b07f93ad69b7
Dominik <dominik.teodorczyk1@gmail.com>
2023-10-23 22:10:54 +0200

    docstrings

--- src/scrapers/superbet.py
+++ src/scrapers/superbet.py
@@ -1,13 +1,21 @@
+"""
+Module for the SuperbetScraper classes, a specialized scrapers for collecting
+data from Superbet.
+
+Classes:
+---------
+SuperbetScraper: A specialized scraper for collecting data from Superbet,
+    inherits from Scraper.
+SuperbetTwoWayBets(SuperbetScraper):A class representing a scraper for
+    collecting two-way sports betting data from Superbet.
+SuperbetThreeWayBets(SuperbetScraper): A class representing a scraper for
+    collecting three-way sports betting data from Superbet.
+"""
+import traceback
+import time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support import expected_conditions as EC
 from scrapers.base import Scraper
-import time
-from selenium import webdriver
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from utils.parsers import SuperbetParser
 from utils.events import (
     TwoWayBetEvent,
@@ -16,10 +24,20 @@ from utils.events import (
     ThreeWayBetEventsTable,
 )
 from utils.technical import setup_logger
-import traceback
-
 
 class SuperbetScraper(Scraper):
+    """
+    A specialized scraper for collecting data from Superbet, inherits
+    from Scraper.
+
+    Attributes:
+    -----------
+    - site_path (str): The URL of the Superbet website.
+    - events_objects (list): A dictionary to store event objects
+        collected from Superbet.
+    - logging (Logger): The logger for handling log messages.
+    """
+
     def __init__(self, site_path: str) -> None:
         super().__init__(site_path)
         self.events_objects: list = []
@@ -27,6 +45,9 @@ class SuperbetScraper(Scraper):
         self.logging.info(f"Starting to collect data: {self.site_path}")
 
     def close_cookie_msg(self):
+        """
+        Close the cookie message on the webpage.
+        """
         try:
             close_button = self.driver.find_element(
                 By.XPATH, '//*[@id="onetrust-accept-btn-handler"]'
@@ -38,6 +59,9 @@ class SuperbetScraper(Scraper):
             self.logging.error(f"Unknown bug, more here: {e}")
 
     def get_whole_site(self):
+        """
+        Scroll through the entire webpage to ensure all content is loaded.
+        """
         last_height = self.driver.execute_script(
             "return document.body.scrollHeight"
         )
@@ -54,26 +78,63 @@ class SuperbetScraper(Scraper):
             last_height = new_height
         self.driver.execute_script("window.scrollTo(0, 0);")
 
-    def get_segments(self):
-        pass
-
     def get_events_from_site(self):
+        """
+        Attempts to collect events by closing cookie messages, scrolling
+        through the entire site.
+        """
         try:
             self.close_cookie_msg()
             self.get_whole_site()
             self.get_whole_site()
-            self.get_segments()
         except Exception as e:
             self.logging.error(f"Unknown bug, more here: {e}")
         self.logging.info(f"Events collected: {self.site_path}")
 
 
 class SuperbetTwoWayBets(SuperbetScraper):
+    """
+    A class representing a scraper for collecting two-way sports betting
+    data from Superbet.
+
+    Inherits from:
+    --------------
+    SuperbetScraper
+
+    Attributes:
+    ------------
+    events_data (TwoWayBetEventsTable): An instance of the
+    TwoWayBetEventsTable class for storing and managing two-way
+    sports betting event data.
+
+    Parameters:
+    -----------
+    site_path (str): The URL of the Superbet webpage to scrape.
+    """
+
     def __init__(self, site_path: str) -> None:
         super().__init__(site_path)
         self.events_data = TwoWayBetEventsTable("SUPERBET")
 
     def get_events_values(self, result_queue):
+        """
+        Collects and processes two-way sports betting data from
+        the Superbet webpage.
+
+        This method scrapes and extracts relevant information
+        from the Superbet webpage for two-way sports betting events.
+        The collected data is then formatted and added to
+        the TwoWayBetEventsTable using the SuperbetParser.
+
+        Parameters:
+        -----------
+        result_queue (Queue): A queue to store the result
+        (TwoWayBetEventsTable) for further processing.
+
+        Returns:
+        --------
+        None
+        """
         self.get_events_from_site()
         height = self.driver.execute_script("return window.scrollY;")
         self.driver.execute_script("window.scrollTo(0, 0);")
@@ -114,12 +175,14 @@ class SuperbetTwoWayBets(SuperbetScraper):
                             )
                         )
                         self.events_objects.append(f"{home_name}{away_name}")
-                except NoSuchElementException as e:
+                except NoSuchElementException:
                     pass
                 except Exception as e:
                     exception_message = str(e)
                     traceback_str = traceback.format_exc()
-                    self.logging.error(f"Unknown bug, more here: {traceback_str} {exception_message}")
+                    self.logging.error(
+                        f"Unknown bug, more here: {traceback_str} {exception_message}"
+                    )
             self.driver.execute_script(
                 f"window.scrollTo(0, window.scrollY + {4000});"
             )
@@ -132,12 +195,50 @@ class SuperbetTwoWayBets(SuperbetScraper):
         self.driver.quit()
         result_queue.put(self.events_data)
 
+
 class SuperbetThreeWayBets(SuperbetScraper):
+    """
+    A class representing a scraper for collecting three-way sports betting
+    data from Superbet.
+
+    Inherits from:
+    --------------
+    SuperbetScraper
+
+    Attributes:
+    ------------
+    events_data (ThreeWayBetEventsTable): An instance of the
+    ThreeWayBetEventsTable class for storing and managing three-way
+    sports betting event data.
+
+    Parameters:
+    -----------
+    site_path (str): The URL of the Superbet webpage to scrape.
+    """
+
     def __init__(self, site_path: str) -> None:
         super().__init__(site_path)
         self.events_data = ThreeWayBetEventsTable("SUPERBET")
 
     def get_events_values(self, result_queue):
+        """
+        Collects and processes three-way sports betting data from
+        the Superbet webpage.
+
+        This method scrapes and extracts relevant information
+        from the Superbet webpage for three-way sports betting events.
+        The collected data is then formatted and added to
+        the ThreeWayBetEventsTable using the SuperbetParser.
+
+        Parameters:
+        -----------
+        result_queue (Queue): A queue to store the result
+        (ThreeWayBetEventsTable) for further processing.
+
+        Returns:
+        --------
+        None
+        """
         self.get_events_from_site()
         height = self.driver.execute_script("return window.scrollY;")
         self.driver.execute_script("window.scrollTo(0, 0);")
@@ -182,12 +283,14 @@ class SuperbetThreeWayBets(SuperbetScraper):
                             )
                         )
                         self.events_objects.append(f"{home_name}{away_name}")
-                except NoSuchElementException as e:
+                except NoSuchElementException:
                     pass
                 except Exception as e:
                     exception_message = str(e)
                     traceback_str = traceback.format_exc()
-                    self.logging.error(f"Unknown bug, more here: {traceback_str} {exception_message}")
+                    self.logging.error(
+                        f"Unknown bug, more here: {traceback_str} {exception_message}"
+                    )
             self.driver.execute_script(
                 f"window.scrollTo(0, window.scrollY + {4000});"
             )
