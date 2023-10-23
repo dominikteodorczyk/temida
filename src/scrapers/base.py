725e21dc4bb3ed883e5e0d8d038c0a5d2168e70f
Dominik <dominik.teodorczyk1@gmail.com>
2023-10-23 22:12:09 +0200

    docstrings and pylint scanning

--- src/scrapers/base.py
+++ src/scrapers/base.py
@@ -1,27 +1,62 @@
+"""
+Module for the Scraper class, a base class for web scraping using Selenium.
+
+Classes:
+---------
+Scraper: A base class for web scraping using Selenium.
+"""
+
 from selenium import webdriver
 from selenium.common.exceptions import SessionNotCreatedException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from utils.technical import setup_logger
 
 
 class Scraper:
+    """
+    A base class for web scraping using Selenium.
+
+    Attributes:
+    -----------
+    - site_path (object): The path or URL of the website to scrape.
+    - driver (webdriver.Chrome): The Chrome WebDriver for interacting
+        with the website.
+    - logging (Logger): The logger for handling log messages.
+    - wait (WebDriverWait): The WebDriverWait object for waiting for
+        elements to load.
+    """
+
     def __init__(self, site_path: object) -> None:
         self.site_path = site_path
         options = Options()
         options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=options)
+        self.logging = setup_logger(name="BASE", print_logs=True)
+
         try:
-            if self.site_path != None:
+            if self.site_path is not None:
                 self.driver.get(site_path)
                 self.wait = WebDriverWait(self.driver, 20)
             else:
                 self.driver.quit()
                 pass
         except SessionNotCreatedException as e:
+            self.logging.info(f"URL error: {e}")
             self.driver.quit()
-            pass
-
 
     @classmethod
     def init_with(cls, site_path):
+        """
+        Class method to create an instance of the Scraper class.
+
+        Parameters:
+        -----------
+        - cls (class): The class (Scraper) that this method belongs to.
+            site_path: The path or URL of the website to scrape.
+
+        Returns:
+        --------
+        Scraper: An instance of the Scraper class.
+        """
         return cls(site_path)
--- src/scrapers/betclic.py
+++ src/scrapers/betclic.py
@@ -1,8 +1,21 @@
+"""
+Module for the BetclicScraper classes, a specialized scrapers for collecting
+data from Betclic.
+
+Classes:
+---------
+BetclicScraper: A specialized scraper for collecting data from Betclic,
+    inherits from Scraper.
+BetclicTwoWayBets(BetclicScraper):A class representing a scraper for
+    collecting two-way sports betting data from Betclic.
+BetclicThreeWayBets(BetclicScraper): A class representing a scraper for
+    collecting three-way sports betting data from Betclic.
+"""
+
+import time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support import expected_conditions as EC
 from scrapers.base import Scraper
-import time
 from utils.parsers import BetclicParser
 from utils.events import (
     TwoWayBetEvent,
@@ -14,6 +27,20 @@ from utils.technical import setup_logger
 
 
 class BetclicScraper(Scraper):
+    """
+    A specialized scraper for collecting data from Betclic, inherits
+    from Scraper.
+
+    Attributes:
+    -----------
+    - site_path (str): The URL of the Betclic website.
+    - competition_boxes (list): A list to store competition boxes
+        on the Betclic webpage.
+    - events_objects (dict): A dictionary to store event objects
+        collected from Betclic.
+    - logging (Logger): The logger for handling log messages.
+    """
+
     def __init__(self, site_path: str) -> None:
         super().__init__(site_path)
         self.competition_boxes: list = []
@@ -22,6 +49,9 @@ class BetclicScraper(Scraper):
         self.logging.info(f"Starting to collect data: {self.site_path}")
 
     def close_cookie_msg(self):
+        """
+        Close the cookie message on the webpage.
+        """
         try:
             close_button = self.driver.find_element(
                 By.XPATH, '//*[@id="popin_tc_privacy_button_2"]'
@@ -33,6 +63,9 @@ class BetclicScraper(Scraper):
             self.logging.error(f"Unknown bug, more here: {e}")
 
     def get_whole_site(self):
+        """
+        Scroll through the entire webpage to ensure all content is loaded.
+        """
         last_height = self.driver.execute_script(
             "return document.body.scrollHeight"
         )
@@ -50,6 +83,9 @@ class BetclicScraper(Scraper):
         time.sleep(1)
 
     def get_segments(self):
+        """
+        Collect and store the segment elements from the current webpage.
+        """
         try:
             boxes = self.driver.find_elements(
                 By.XPATH,
@@ -62,9 +98,12 @@ class BetclicScraper(Scraper):
                 ) or (box.get_attribute("class") == "groupEvents"):
                     self.competition_boxes.append(box)
         except NoSuchElementException as e:
-            self.logging.warning(f"No segment elements")
+            self.logging.warning(f"No segment elements:{e}")
 
     def get_all_events_objects(self):
+        """
+        Collect and store all event objects from the current webpage.
+        """
         for box in self.competition_boxes:
             for event in box.find_elements(
                 By.XPATH, "./div[2]/sports-events-event[*]"
@@ -74,6 +113,11 @@ class BetclicScraper(Scraper):
                 ).text
 
     def get_events_from_site(self):
+        """
+        Attempts to collect events by closing cookie messages, scrolling
+        through the entire site, extracting segments, and finally obtaining
+        all event objects from the segments.
+        """
         try:
             self.close_cookie_msg()
             self.get_whole_site()
@@ -86,13 +130,49 @@ class BetclicScraper(Scraper):
 
 
 class BetclicTwoWayBets(BetclicScraper):
+    """
+    A class representing a scraper for collecting two-way sports betting
+    data from Betclic.
+
+    Inherits from:
+    --------------
+    BetclicScraper
+
+    Attributes:
+    ------------
+    events_data (TwoWayBetEventsTable): An instance of the
+    TwoWayBetEventsTable class for storing and managing two-way
+    sports betting event data.
+
+    Parameters:
+    -----------
+    site_path (str): The URL of the Betclic webpage to scrape.
+    """
+
     def __init__(self, site_path: str) -> None:
         super().__init__(site_path)
         self.events_data = TwoWayBetEventsTable("BETCLIC")
 
-    def get_events_values(self, result_queue):
+    def get_events_values(self, result_queue) -> None:
+        """
+        Collects and processes two-way sports betting data from
+        the Betclic webpage.
+
+        This method scrapes and extracts relevant information
+        from the Betclic webpage for two-way sports betting events.
+        The collected data is then formatted and added to
+        the TwoWayBetEventsTable using the BetclicParser.
+
+        Parameters:
+        -----------
+        result_queue (Queue): A queue to store the result
+        (TwoWayBetEventsTable) for further processing.
+
+        Returns:
+        --------
+        None
+        """
         self.get_events_from_site()
-        # parser = FortunaParsers()
         for event, date in self.events_objects.items():
             try:
                 event_data = {
@@ -119,7 +199,7 @@ class BetclicTwoWayBets(BetclicScraper):
                         event_data, BetclicParser()
                     )
                 )
-            except NoSuchElementException as e:
+            except NoSuchElementException:
                 pass
             except Exception as e:
                 self.logging.error(f"Unknown bug, more here: {e}")
@@ -129,13 +209,49 @@ class BetclicTwoWayBets(BetclicScraper):
 
 
 class BetclicThreeWayBets(BetclicScraper):
+    """
+    A class representing a scraper for collecting three-way sports betting
+    data from Betclic.
+
+    Inherits from:
+    --------------
+    BetclicScraper
+
+    Attributes:
+    ------------
+    events_data (ThreeWayBetEventsTable): An instance of the
+    ThreeWayBetEventsTable class for storing and managing three-way
+    sports betting event data.
+
+    Parameters:
+    -----------
+    site_path (str): The URL of the Betclic webpage to scrape.
+    """
+
     def __init__(self, site_path: str) -> None:
         super().__init__(site_path)
         self.events_data = ThreeWayBetEventsTable("BETCLIC")
 
-    def get_events_values(self, result_queue):
+    def get_events_values(self, result_queue) -> None:
+        """
+        Collects and processes three-way sports betting data from
+        the Betclic webpage.
+
+        This method scrapes and extracts relevant information
+        from the Betclic webpage for three-way sports betting events.
+        The collected data is then formatted and added to
+        the ThreeWayBetEventsTable using the BetclicParser.
+
+        Parameters:
+        -----------
+        result_queue (Queue): A queue to store the result
+        (ThreeWayBetEventsTable) for further processing.
+
+        Returns:
+        --------
+        None
+        """
         self.get_events_from_site()
-        # parser = FortunaParsers()
         for event, date in self.events_objects.items():
             try:
                 event_data = {
@@ -167,7 +283,7 @@ class BetclicThreeWayBets(BetclicScraper):
                         event_data, BetclicParser()
                     )
                 )
-            except NoSuchElementException as e:
+            except NoSuchElementException:
                 pass
             except Exception as e:
                 self.logging.error(f"Unknown bug, more here: {e}")
