953d2f29ea6f71b4736ee057735becdc76629f80
Dominik <dominik.teodorczyk1@gmail.com>
2023-10-23 22:05:41 +0200

    main app init

--- main.py
+++ main.py
@@ -1,44 +1,18 @@
-import time
-from app.webscrper import ScrapersPool
-import pandas as pd
-import os
-from utils.events import *
-from app.arbitrage import *
+"""
+This module contains the main entry point for the application.
+"""
+from app.application import MainOperator
+
 
 def main():
-    data_dir = os.path.join(os.getcwd(), "tests", "data")
-    fortuna_board = pd.read_csv(os.path.join(data_dir, "fortuna_data.csv"))
-    sts_board = pd.read_csv(os.path.join(data_dir, "sts_data.csv"))
-    betclic_board = pd.read_csv(os.path.join(data_dir, "betclic_data.csv"))
-    superbet_board = pd.read_csv(os.path.join(data_dir, "superbet_data.csv"))
-    events = pd.read_csv(os.path.join(data_dir, "merged.csv"))
+    """
+    Main function to start the application.
 
-    sts_obj = ThreeWayBetEventsTable('STS')
-    sts_obj.data = sts_board
+    Creates an instance of the MainOperator class from the
+    'app.application' module.
+    """
+    MainOperator()
 
-    fortuna_obj = ThreeWayBetEventsTable('FORTUNA')
-    fortuna_obj.data = fortuna_board
-
-    betclic_obj = ThreeWayBetEventsTable('BETCLIC')
-    betclic_obj.data = betclic_board
-
-    superbet_obj = ThreeWayBetEventsTable('SUPERBET')
-    superbet_obj.data = superbet_board
-
-
-    main_obj = MainEventsBoard()
-    main_obj.put_data(sts_obj)
-    main_obj.put_data(fortuna_obj)
-    main_obj.put_data(betclic_obj)
-    main_obj.put_data(superbet_obj)
-
-    main_obj.events_table = events
-
-    arbitrage_obj = Arbitrage(main_obj)
-    arbitrage_obj.calculate_arbitage()
 
 if __name__ == "__main__":
     main()
-
-# 2196 rows x 4 columns
-# 1416
