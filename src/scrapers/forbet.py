1d7a5cab77a9ba4a13bf05a0b64209b478dbb974
Dominik <dominik.teodorczyk1@gmail.com>
2023-12-13 16:15:11 +0100

    cookies msg closing

--- src/scrapers/forbet.py
+++ src/scrapers/forbet.py
@@ -22,12 +22,22 @@ class ForbetScraper(Scraper):
         except Exception as e:
             self.logging.error(f"Unknown bug, more here: {e}")
 
-
+    def close_cookies_msg(self):
+        try:
+            confirm_button = self.driver.find_element(
+                By.XPATH, '/html/body/div[1]/div[1]/div/div/div[2]/button[3]'
+            )
+            confirm_button.click()
+        except NoSuchElementException:
+            self.logging.warning("Can't close adult msg")
+        except Exception as e:
+            self.logging.error(f"Unknown bug, more here: {e}")
 
     def get_events_from_site(self):
 
         try:
             self.close_adult_msg()
+            self.close_cookies_msg
         except Exception as e:
             self.logging.error(f"Unknown bug, more here: {e}")
         self.logging.info(f"Events collected: {self.site_path}")
