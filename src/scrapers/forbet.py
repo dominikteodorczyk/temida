a6237d3089621bd54814f7b2064a306a0282c096
Dominik <dominik.teodorczyk1@gmail.com>
2023-12-13 16:14:40 +0100

    adult msg close clicer

--- src/scrapers/forbet.py
+++ src/scrapers/forbet.py
@@ -1,4 +1,7 @@
+import time
 from scrapers.base import Scraper
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 class ForbetScraper(Scraper):
@@ -6,3 +9,25 @@ class ForbetScraper(Scraper):
         super().__init__(site_path)
         self.competition_boxes: list = []
         self.events_objects: dict = {}
+
+
+    def close_adult_msg(self):
+        try:
+            confirm_button = self.driver.find_element(
+                By.XPATH, '//*[@id="adultConfirmation"]/div/button[1]'
+            )
+            confirm_button.click()
+        except NoSuchElementException:
+            self.logging.warning("Can't close adult msg")
+        except Exception as e:
+            self.logging.error(f"Unknown bug, more here: {e}")
+
+
+
+    def get_events_from_site(self):
+
+        try:
+            self.close_adult_msg()
+        except Exception as e:
+            self.logging.error(f"Unknown bug, more here: {e}")
+        self.logging.info(f"Events collected: {self.site_path}")
