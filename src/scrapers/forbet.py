1bb85615fa87aff52461faa4c9cf816800b0a217
Dominik <dominik.teodorczyk1@gmail.com>
2024-01-02 13:26:21 +0100

    problem with getting main segments

--- src/scrapers/forbet.py
+++ src/scrapers/forbet.py
@@ -7,7 +7,8 @@ from selenium.common.exceptions import NoSuchElementException
 class ForbetScraper(Scraper):
     def __init__(self, site_path: str) -> None:
         super().__init__(site_path)
-        self.competition_boxes: list = []
+        self.main_boxes: list = []
+        self.competition_boxes: dict = {}
         self.events_objects: dict = {}
 
 
@@ -33,12 +34,44 @@ class ForbetScraper(Scraper):
         except Exception as e:
             self.logging.error(f"Unknown bug, more here: {e}")
 
+    def get_main_sections(self):
+        """
+        Collect and store the main events segment elements from the
+        current webpage.
+        """
+        height = self.driver.execute_script("return window.scrollY;")
+        self.driver.execute_script("window.scrollTo(0, 0);")
+        while True:
+            try:
+                self.main_boxes = self.main_boxes.append(self.driver.find_elements(
+                    By.XPATH, '//*[@id="__next"]/div/div/main/div/div/div/div/div[1]/section/div/section[*]'
+                ))
+            except NoSuchElementException:
+                self.logging.warning("No segment elements")
+            self.driver.execute_script(
+                f"window.scrollTo(0, window.scrollY + {4000});"
+            )
+            time.sleep(0.01)
+            new_height = self.driver.execute_script("return window.scrollY;")
+            if height == new_height:
+                break
+            height = new_height
+
+
+
 
     def get_events_from_site(self):
 
         try:
             self.close_adult_msg()
             self.close_cookies_msg()
+            self.get_main_sections()
+            print(len(self.main_boxes))
+            for box in self.main_boxes:
+                for event in box.find_elements(
+                    By.XPATH, ".//header/h2/span[1]"
+                ):
+                    print(event.text)
         except Exception as e:
             self.logging.error(f"Unknown bug, more here: {e}")
         self.logging.info(f"Events collected: {self.site_path}")
