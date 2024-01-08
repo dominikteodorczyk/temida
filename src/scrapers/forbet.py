31e8b9227e86bdd93fd869aefaeca6f7457d351e
Dominik <dominik.teodorczyk1@gmail.com>
2024-01-08 17:24:19 +0100

    added forbet scraper main boxex finder

--- src/scrapers/forbet.py
+++ src/scrapers/forbet.py
@@ -25,6 +25,7 @@ class ForbetScraper(Scraper):
 
     def close_cookies_msg(self):
         try:
+            time.sleep(5)
             confirm_button = self.driver.find_element(
                 By.XPATH, '/html/body/div[1]/div[1]/div/div/div[2]/button[3]'
             )
@@ -39,25 +40,15 @@ class ForbetScraper(Scraper):
         Collect and store the main events segment elements from the
         current webpage.
         """
-        height = self.driver.execute_script("return window.scrollY;")
-        self.driver.execute_script("window.scrollTo(0, 0);")
-        while True:
-            try:
-                self.main_boxes = self.main_boxes.append(self.driver.find_elements(
-                    By.XPATH, '//*[@id="__next"]/div/div/main/div/div/div/div/div[1]/section/div/section[*]'
-                ))
-            except NoSuchElementException:
-                self.logging.warning("No segment elements")
-            self.driver.execute_script(
-                f"window.scrollTo(0, window.scrollY + {4000});"
+        time.sleep(5)
+        try:
+            _main_boxes = self.driver.find_elements(
+                By.XPATH, '//*[@id="__next"]/div/div/main/div/div/div/div/div[1]/section/div/section[*]'
             )
-            time.sleep(0.01)
-            new_height = self.driver.execute_script("return window.scrollY;")
-            if height == new_height:
-                break
-            height = new_height
-
-
+            for box in _main_boxes:
+                self.main_boxes.append(box)
+        except NoSuchElementException:
+            self.logging.warning("No segment elements")
 
 
     def get_events_from_site(self):
@@ -66,12 +57,8 @@ class ForbetScraper(Scraper):
             self.close_adult_msg()
             self.close_cookies_msg()
             self.get_main_sections()
-            print(len(self.main_boxes))
-            for box in self.main_boxes:
-                for event in box.find_elements(
-                    By.XPATH, ".//header/h2/span[1]"
-                ):
-                    print(event.text)
         except Exception as e:
             self.logging.error(f"Unknown bug, more here: {e}")
+
+
         self.logging.info(f"Events collected: {self.site_path}")
